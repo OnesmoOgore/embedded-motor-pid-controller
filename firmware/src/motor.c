a2313d53d4698e945c86588ff55c510c40e07810
Onesmo Ogore <OnesmoOgore@gmail.com>
2025-11-17 20:13:16 -0500

    Create motor.c
    Motor code initial motor.c commit

---
+++ firmware/src/motor.c
@@ -0,0 +1,19 @@
+/*
+ * @file    motor.c
+ * @author  Onesmo Ogore
+ * @date    11/17/2025
+ * @brief   Source file for the DC motor control module.
+ * 
+ * SPDX-License-Identifier: MIT
+ *
+ * @section DESCRIPTION
+ * This file provides the implementation for initializing, controlling speed, 
+ * and reading encoder feedback from the motor hardware.
+ */
+
+// Includes
+#include "motor.h"
+
+// Motor code
+
+// Motor code
