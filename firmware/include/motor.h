ffab256229a9f1851def9aa2b315d8bd94aaa0b3
Onesmo Ogore <OnesmoOgore@gmail.com>
2025-11-17 20:21:32 -0500

    Create motor.h
    Initial motor.h commit

---
+++ firmware/include/motor.h
@@ -0,0 +1,12 @@
+/*
+ * @file    motor.h
+ * @author  Onesmo Ogore
+ * @date    11/17/2025
+ * @brief   Header file for the DC motor control module definitions.
+ * 
+ * SPDX-License-Identifier: MIT
+ *
+ * @section DESCRIPTION
+ * This file contains the function prototypes, constants, and data types 
+ * required to initialize and control the motor hardware peripherals.
+ */
