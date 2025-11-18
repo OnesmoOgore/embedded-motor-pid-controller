3355f08ff31af769ed79f1f1659cd11ce3c1fec6
Onesmo Ogore <OnesmoOgore@gmail.com>
2025-11-17 20:17:43 -0500

    Create pid.c
    PID code initial pid.c commit

---
+++ firmware/src/pid.c
@@ -0,0 +1,17 @@
+/*
+ * @file    pid.c
+ * @author  Onesmo Ogore
+ * @date    11/17/2025
+ * @brief   Implementation of the Proportional-Integral-Derivative (PID) algorithm.
+ * 
+ * SPDX-License-Identifier: MIT
+ *
+ * @section DESCRIPTION
+ * This file provides functions for initializing the PID controller structure, 
+ * calculating the control output based on feedback, and resetting integral windup.
+ */
+
+// Includes
+#include "pid.h"
+
+// PID code
